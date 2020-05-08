1fbc8d400b709fa07ce4dac9a4837c3092106936
hongweifei <13071580229@163.com>
2020-05-08 20:50:55 +0800

    2020,5,8

--- complie.bat
+++ complie.bat
@@ -3,7 +3,7 @@
 
 
 
-tsc --outFile main.js control/keyboard.ts event/queue.ts event/touch.ts event/mouse.ts event/joy_stick.ts event/event_manager.ts graphics/rect.ts graphics/path.ts graphics/layer.ts graphics/scene.ts graphics/renderer.ts graphics/sprite.ts game/game.ts main.ts
+tsc --outFile main.js event/keyboard.ts event/queue.ts event/touch.ts event/mouse.ts event/joy_stick.ts event/event_manager.ts graphics/rect.ts graphics/path.ts graphics/layer.ts graphics/scene.ts graphics/renderer.ts graphics/sprite.ts game/game.ts main.ts
 
 
 pause
