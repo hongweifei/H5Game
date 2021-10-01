b60ddef1d9bd6ef8bc9a8ea9956db72b21321ebd
flown <41979563+hongweifei@users.noreply.github.com>
2021-10-01 21:39:17 +0800

    Add files via upload
    2021,10,1

--- compile.bat
+++ compile.bat
@@ -3,7 +3,22 @@
 
 
 
-tsc --outFile main.js event/keyboard.ts event/queue.ts event/touch.ts event/mouse.ts event/joy_stick.ts event/event_manager.ts graphics/rect.ts graphics/path.ts graphics/layer.ts graphics/scene.ts graphics/renderer.ts graphics/sprite.ts game/game.ts main.ts
+tsc --outFile main.js ^
+event/keyboard.ts ^
+event/keyboard_kaios.ts ^
+event/queue.ts ^
+event/touch.ts ^
+event/mouse.ts ^
+event/joy_stick.ts ^
+event/event_manager.ts ^
+graphics/rect.ts ^
+graphics/path.ts ^
+graphics/layer.ts ^
+graphics/scene.ts ^
+graphics/renderer.ts ^
+graphics/sprite.ts ^
+game/game.ts ^
+main.ts
 
 
 pause
